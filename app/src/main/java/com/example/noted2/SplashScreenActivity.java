1560614118143bd632450ecc6ac11616c381e144
pitagorinaunuka <madagaskar111>
2020-04-26 01:12:27 +0200

    Splash screen first display

--- app/src/main/java/com/example/noted2/SplashScreenActivity.java
+++ app/src/main/java/com/example/noted2/SplashScreenActivity.java
@@ -1,13 +1,55 @@
 package com.example.noted2;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler handler;
+    ImageView splashImg;
+    TextView splashText;
+    Button startBtn, signBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        splashImg = findViewById(R.id.splash_img);
+        splashText = findViewById(R.id.splashText);
+        signBtn = findViewById(R.id.sign_btn);
+        startBtn = findViewById(R.id.start_btn);
+
+        ((View) startBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(a);
+            }
+        });
+
+        handler=new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },600000);
+
     }
+
+
+
+
+
+
 }
