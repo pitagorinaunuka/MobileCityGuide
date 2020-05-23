f41d739ca4fe113f77e16494d7075550fa71e6fd
idabajrami <39003040+pitagorinaunuka@users.noreply.github.com>
2020-05-24 00:07:20 +0200

    Changes

--- app/src/main/java/com/example/noted2/SplashScreenActivity.java
+++ app/src/main/java/com/example/noted2/SplashScreenActivity.java
@@ -2,7 +2,7 @@ package com.example.noted2;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,10 +43,11 @@ public class SplashScreenActivity extends AppCompatActivity {
             }
         });
 
+
         ((View) signBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent s = new Intent(SplashScreenActivity.this, SingInActivity.class);
+                Intent s = new Intent(SplashScreenActivity.this, SingUpActivity.class);
                 startActivity(s);
             }
         });
