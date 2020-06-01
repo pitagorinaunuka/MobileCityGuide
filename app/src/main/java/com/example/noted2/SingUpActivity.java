31f9a4d82bae7b4abac0f7b269aa00f3ae5328b8
idabajrami <39003040+pitagorinaunuka@users.noreply.github.com>
2020-06-01 11:18:52 +0200

    SignUp activity correction

--- app/src/main/java/com/example/noted2/SingUpActivity.java
+++ app/src/main/java/com/example/noted2/SingUpActivity.java
@@ -3,6 +3,7 @@ package com.example.noted2;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,17 +14,26 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SingUpActivity extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
     ImageView splashImg;
-    EditText emailInput, passInput;
+    EditText emailInput, passInput, nameInput;
     Button signInBtn;
     TextView backBtn;
     FirebaseAuth mFirebaseAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +41,22 @@ public class SingUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        nameInput = findViewById(R.id.name_sign_input);
         splashImg = findViewById(R.id.splash_img);
         emailInput = findViewById(R.id.email_sign_input);
         passInput = findViewById(R.id.pass_sign_input);
         backBtn = findViewById(R.id.back_sign_text);
         signInBtn = findViewById(R.id.sign_btn);
+
         signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailInput.getText().toString();
-                String pwd = passInput.getText().toString();
+
+                final String email = emailInput.getText().toString();
+                final String pwd = passInput.getText().toString();
+                final String name = nameInput.getText().toString();
 
                 if (email.isEmpty()) {
                     emailInput.setError("Please enter your email.");
@@ -56,8 +72,20 @@ public class SingUpActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
                                 Toast.makeText(SingUpActivity.this, "Sing up failed. Please try again.", Toast.LENGTH_SHORT).show();
-                            }
-                            else {
+                            } else {
+                                userID = mFirebaseAuth.getCurrentUser().getUid();
+                                DocumentReference documentReference = fStore.collection("users").document(userID);
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("email", email);
+                                user.put("password", pwd);
+                                user.put("name", name);
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "onSuccess: the profile is created "+ userID);
+                                    }
+                                });
+
                                 startActivity(new Intent(SingUpActivity.this, MainActivity.class));
                             }
                         }
